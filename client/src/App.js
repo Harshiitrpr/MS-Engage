68ca74726427fa810b3fb66cc2d63e979abc3451
Harshiitrpr <2019csb1089@iitrpr.ac.in>
2021-07-06 21:24:44 +0530

    added authentication

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 // import CreateRoom from "./routes/CreateRoom";
 // import Room from "./routes/Room";
+import PrivateRoute from "./components/firebase/PrivateRoute"
+import { AuthProvider } from "./contexts/AuthContext"
+import Signup from "./components/firebase/Signup";
+import Login from "./components/firebase/Login";
 import HomeRoute from "./components/pageComponents/home/HomeRoute"
 import RoomRoute from "./components/pageComponents/room/RoomRoute"
 import CheckOut from "./components/pageComponents/exit/ExitRoute"
@@ -9,11 +13,15 @@ import CheckOut from "./components/pageComponents/exit/ExitRoute"
 function App() {
   return (
     <BrowserRouter>
+    <AuthProvider>
       <Switch>
-        <Route path="/" exact component={HomeRoute} />
-        <Route path="/room/:roomID" component={RoomRoute}/>
+        <PrivateRoute exact path="/" exact component={HomeRoute} />
+        <PrivateRoute path="/room/:roomID" component={RoomRoute}/>
+        <Route path="/signup" component={Signup} />
+        <Route path="/login" component={Login} />
         <Route path="/exit" component={CheckOut} />
       </Switch>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
--- client/src/components/pageComponents/home/HomeRoute.js
+++ client/src/components/pageComponents/home/HomeRoute.js
@@ -1,18 +1,46 @@
-import React from "react";
+import React, { useState } from "react"
 import { v1 as uuid } from "uuid";
+import { Card, Button, Alert } from "react-bootstrap"
+import { useAuth } from "../../../contexts/AuthContext"
+import { Link, useHistory } from "react-router-dom"
 
-const HomeRoute = (props) => {
-    const create = () => {
-        const id = uuid();
-        props.history.push(`/room/${id}`);
+export default function Dashboard() {
+  const [error, setError] = useState("")
+  const { currentUser, logout } = useAuth()
+  const history = useHistory()
+
+  const create = () => {
+    const id = uuid();
+    history.push(`/room/${id}`);
+}
+
+  async function handleLogout() {
+    setError("")
+
+    try {
+      await logout()
+      history.push("/login")
+    } catch {
+      setError("Failed to log out")
     }
+  }
 
-    return (
-        <div>
-            <section><button onClick={create}>Create room</button></section>
-        </div>
-        
-    );
-};
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Profile</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <strong>Email:</strong> {currentUser.email}
+          <button onClick={create}>Create room</button>
+        </Card.Body>
+      </Card>
+      <div className="w-100 text-center mt-2">
+        <Button variant="link" onClick={handleLogout}>
+          Log Out
+        </Button>
+      </div>
+    </>
+  )
+}
 
-export default HomeRoute;
