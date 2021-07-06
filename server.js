d564bb788eb02aa4d18d7b5192647bf001b674aa
Harshiitrpr <2019csb1089@iitrpr.ac.in>
2021-07-06 18:14:53 +0530

    please run

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ io.on('connection', socket => {
 });
 
 if(process.env.PROD){
-    app.use(exporess.static(path.join(__dirname, './client/build')));
+    app.use(express.static(path.join(__dirname, './client/build')));
     app.get('*', (req, res) =>{
         res.sendFile(path.join(__dirname,'./client/build/index.html'));
     });
